4ef966b7ec15a5a994f4710f62923fa813676074
Emanuele Cochi <emanuelecochi@gmail.com>
2018-06-20 13:41:06 +0200

    delete timesleep

--- IoTArduino/IoTArduino.ino
+++ IoTArduino/IoTArduino.ino
@@ -13,9 +13,9 @@
 DHT_Unified dht(DHTPIN, DHTTYPE);
 
 // Cayenne authentication info. This should be obtained from the Cayenne Dashboard.
-char username[] = "MQTT_USERNAME";
-char password[] = "MQTT_PASSWORD";
-char clientID[] = "CLIENT_ID";
+char username[] = "16831f80-745e-11e8-8fb7-c3a557c9635e";
+char password[] = "7a1be4aa7b26b54f5b0f01e3ce781e1722d79f55";
+char clientID[] = "bcb42d80-745f-11e8-8458-69a620124a87";
 
 unsigned long lastMillis = 0;
 
@@ -32,15 +32,11 @@ void loop() {
 
 CAYENNE_OUT_DEFAULT()
 {
-  //Publish data every 1 seconds (1000 milliseconds). Change this value to publish at a different interval.
-  if(millis() - lastMillis > 1000) {
-    lastMillis = millis();
-    sensors_event_t event;  
-    dht.temperature().getEvent(&event);
-    Cayenne.celsiusWrite(1, event.temperature);
-    dht.humidity().getEvent(&event);
-    Cayenne.virtualWrite(2, event.relative_humidity, "rel_hum", "p");
-  }
+  sensors_event_t event;  
+  dht.temperature().getEvent(&event);
+  Cayenne.celsiusWrite(1, event.temperature);
+  dht.humidity().getEvent(&event);
+  Cayenne.virtualWrite(2, event.relative_humidity, "rel_hum", "p");
 }
 
 CAYENNE_IN(LIGHT_CHANNEL)
